312602c49f1240f168c94ea5006555987b92352b
steven <steven.bloes@mail.com>
2026-06-22 16:31:41 +0200

    added supabase login

--- js/tracker.js
+++ js/tracker.js
@@ -1,3 +1,67 @@
+
+const supabaseUrl = 'https://odhvvyovhqdghxdnljkt.supabase.co'
+const supabaseKey = 'sb_publishable_4iY1n-fsStaHmqH76m5ecA_9DWglVOT'
+
+// Create client
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey)
+
+// Check if user is already logged in
+async function checkUser() {
+  const { data: { session } } = await supabaseClient.auth.getSession()
+
+  if (session) {
+    showApp()
+  }
+}
+
+checkUser()
+
+// SIGN UP
+async function signUp() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabaseClient.auth.signUp({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+  } else {
+    alert("Check your email to confirm signup!")
+  }
+}
+
+// LOGIN
+async function login() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+  } else {
+    showApp()
+  }
+}
+
+// LOGOUT
+async function logout() {
+  await supabaseClient.auth.signOut()
+  location.reload()
+}
+
+// UI switch
+function showApp() {
+  document.getElementById('auth').style.display = 'none'
+  document.getElementById('app').style.display = 'block'
+}
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("video");
 const resultText = document.getElementById("result");
