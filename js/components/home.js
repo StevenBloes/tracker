b8fe8542999b2e249ca2c188e75f3d14e60857cc
StevenBloes <steven.bloes@mail.com>
2026-06-23 20:38:39 +0200

    adjusted login

--- js/components/home.js
+++ js/components/home.js
@@ -1,31 +1,83 @@
 export const title = "KLA W32 - Startpagina";
 
-function createWelcomeText(){
-	return "Welcome";
+const supabaseUrl = 'https://odhvvyovhqdghxdnljkt.supabase.co'
+const supabaseKey = 'sb_publishable_4iY1n-fsStaHmqH76m5ecA_9DWglVOT'
+
+// Create client
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey)
+
+// Check if user is already logged in
+async function checkUser() {
+  const { data: { session } } = await supabaseClient.auth.getSession()
+
+  if (session) {
+    showApp()
+  }
 }
+
+checkUser()
+
+// SIGN UP
+async function signUp() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabaseClient.auth.signUp({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+  } else {
+    alert("Check your email to confirm signup!")
+  }
+}
+
+// LOGIN
+async function login() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const { data, error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  })
+
+  if (error) {
+    alert(error.message)
+  } else {
+    showApp()
+  }
+}
+
+// LOGOUT
+async function logout() {
+  await supabaseClient.auth.signOut()
+  location.reload()
+}
+
+// UI switch
+function showApp() {
+  document.getElementById('auth').style.display = 'none'
+  document.getElementById('app').style.display = 'block'
+}
+
 
 export function render() {
   return `
-    <h1 id="welcome">${createWelcomeText()}</h1>
-
-    <button id="btnPlanning">Planning</button>
-    <button id="btnStock">Stock Management</button>
-    <button id="btnQuality">Quality Management</button>
+    <div id="auth">
+      <input id="email" type="email" placeholder="Email"><br><br>
+      <input id="password" type="password" placeholder="Password"><br><br>
+      <button onclick="signUp()">Sign Up</button>
+      <button onclick="login()">Login</button>
+  </div>
   `;
 }
 
 export function init(root) {
-  root.querySelector("#btnPlanning").onclick = () => {
-    window.location.hash = "#/planning";
-  };
-
-  root.querySelector("#btnStock").onclick = () => {
-    window.location.hash = "#/stock";
-  };
-
-  root.querySelector("#btnQuality").onclick = () => {
-    window.location.hash = "#/";
-  };
+  root.querySelector("#btnSignUp").onclick = () => { signUp(); };
+  root.querySelector("#btnLogin").onclick = () => { login(); };
 }
 
 export function destroy() {}
---
+++ js/components/stock/search.js
@@ -0,0 +1,13 @@
+export const title = "KLA W32 - Pallet Stock";
+
+export function render() {
+  return ``;
+};
+
+export function init(root) {
+
+}
+
+export function destroy() {
+
+}
